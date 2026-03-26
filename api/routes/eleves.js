4de96c79256c92463ce75d5cc6d06fc12a5a3adc
imran <pdmpppdm@gmail.com>
2026-03-26 09:25:13 +0100

    prof referent, affectation auto et manuelle + routes

--- api/routes/eleves.js
+++ api/routes/eleves.js
@@ -8,17 +8,20 @@ const router = express.Router();
 const eleveController = require('../controllers/eleveController');
 const moyenneController = require('../controllers/moyenneController');
 const optionController = require('../controllers/optionController');
+const professeurController = require('../controllers/professeurController');
 const verifyToken = require('../middlewares/verifyToken');
 const checkRole = require('../middlewares/checkRole');
 const upload = require('../middlewares/upload');
 
-router.get('/:id/options',  verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), optionController.getByEleve);
-router.get('/:id/moyennes', verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), moyenneController.getByEleve);
-router.post('/import',      verifyToken, checkRole('secretariat'), upload.single('fichier'), eleveController.importCSV);
-router.get('/',             verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), eleveController.getAll);
-router.get('/:id',          verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), eleveController.getById);
-router.post('/',            verifyToken, checkRole('secretariat'),                            eleveController.create);
-router.put('/:id',          verifyToken, checkRole('secretariat'),                            eleveController.update);
-router.delete('/:id',       verifyToken, checkRole('secretariat'),                            eleveController.remove);
+router.get('/:id/options',        verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), optionController.getByEleve);
+router.get('/:id/moyennes',       verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), moyenneController.getByEleve);
+router.post('/:id/referent/auto', verifyToken, checkRole('secretariat'                           ), professeurController.affecterAuto);
+router.post('/:id/referent',      verifyToken, checkRole('secretariat'                           ), professeurController.affecter);
+router.post('/import',            verifyToken, checkRole('secretariat'), upload.single('fichier'),  eleveController.importCSV);
+router.get('/',                   verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), eleveController.getAll);
+router.get('/:id',                verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), eleveController.getById);
+router.post('/',                  verifyToken, checkRole('secretariat'),                            eleveController.create);
+router.put('/:id',                verifyToken, checkRole('secretariat'),                            eleveController.update);
+router.delete('/:id',             verifyToken, checkRole('secretariat'),                            eleveController.remove);
 
 module.exports = router;
---
+++ api/routes/professeurs.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Routes des professeurs
+ * @module routes/professeurs
+ */
+
+const express = require('express');
+const router = express.Router();
+const professeurController = require('../controllers/professeurController');
+const verifyToken = require('../middlewares/verifyToken');
+const checkRole = require('../middlewares/checkRole');
+
+router.get('/:id/eleves',         verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), professeurController.getEleves);
+router.get('/',                   verifyToken, checkRole('secretariat', 'proviseur', 'professeur'), professeurController.getAll);
+
+module.exports = router;
